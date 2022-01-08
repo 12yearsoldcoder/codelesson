a216361d6155fdad10b9a675e473eb357ae07ec9
12yearsoldcoder <84294457+12yearsoldcoder@users.noreply.github.com>
2022-01-08 17:54:32 +0900

    Add files via upload
    20210108

---
+++ 백준 9498.py
@@ -0,0 +1,11 @@
+testscore = int(input())
+if testscore >= 90:
+    print('A')
+elif testscore >= 80:
+    print('B')
+elif testscore >= 70:
+    print('C')
+elif testscore >= 60:
+    print('D')
+else:
+    print('F')
