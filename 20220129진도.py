2660b326f51eaca677b8445bcf1aee492bedf85a
마루코딩함 <84294457+12yearsoldcoder@users.noreply.github.com>
2022-01-29 17:53:19 +0900

    Add files via upload

---
+++ 20220129진도.py
@@ -0,0 +1,85 @@
+# 리스트
+# a = [1,2,3,4,5]
+# print(a[6])
+
+# a = [1,2,3]
+# print(a[0] + a[2])
+# print(a[-2])
+# a = [1,2,3,['a','b','c']]
+# print(a[3])
+# print(a[3][0])
+# print(a[-1])
+# a = [1,2,['a','b',['Life','is']]]
+# print(a[2][2][0])
+# a = [1,2,3,4,5]
+# print(a[0:2])
+# b = a[:2]
+# print(b)
+# c = a[2:]
+# print(c)
+# a = [1,2,3,['a','b','c'],4,5]
+# print(a[2:5])
+# print(a[3][:2])
+# a = [1,2,3]
+# print(a[2]+"hi")
+# print(str(a[2])+"hi")
+# a = [1,2,3]
+# print(a)
+# a[2] = 4
+# print(a)
+# a[1] = 5
+# print(a)
+# del a[1]
+# print(a)
+# del a[1:]
+# print(a)
+# a = [1,2,3]
+# a.append(4)
+# print(a)
+# b = []
+# a = int(input())
+# b.append(a)
+# a = [1,2,3]
+# a.append(4)
+# print(a)
+# a = [1,4,3,2]
+# print(a)
+# a.sort()
+# print(a)
+# a = ['a','c','b']
+# a.reverse()
+# print(a)
+# a = [1,4,3,2]
+# print(a)
+# a.sort(reverse=True)
+# print(a)
+# a.sort(reverse=False)
+# print(a)
+# a.sort()
+# print(a)
+#a = [1, 2, 3]
+# #리스트안에 없어서 에러! 에잇!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+#print(a)
+#a.insert(1,7)
+#print(a)
+#a = [1,2,3,1,2,3]
+#a.remove(3)
+#print(a)
+#a.remove(3)
+#print(a)
+# a = [1,2,3]
+# a.remove(2)
+# print(a)
+# a = [4,5,6]
+# a.pop(2)
+# print(a)
+# a.pop(0)
+# print(a)
+# a=[1,2,3,1]
+# print(a.count(1))
+# print(a.count(0))
+# print(a.count(2))
+a = [1,2,3]
+print(a)
+a.extend([4,5])
+print(a)
--- 369369369369369369369.py
+++ 369369369369369369369.py
@@ -1,8 +1,59 @@
-a = 0
-b = {3, 6, 9}
-for a in b:
-    print("*")
-    a += 1
-    if a not in b:
-        a += 1
-        print(a)
+# a = 0
+# b = {3, 6, 9}
+#
+# while a in b:
+#    print("*")
+#    a += 1
+#    if a not in b:
+#        a += 1
+#        print(a)
+
+
+# a = 0
+# b = [3,6,9]
+# for i in range(1,101):
+#    a += 1
+#    if a in b:
+#        print("*")
+#        if a in b:
+#            print("**")
+# a = 0
+# b = [3,6,9]
+# for i in range(1,101):
+#     a += 1
+#     while a in b:
+#         a += 1
+#         while a in b:
+#             print("**")
+#         print("*")
+#     print(a)
+
+
+# for i in range(1, 101):
+#     if 3 == i % 10:
+#         if 6 == i // 10:
+#             print("**")
+#         print("*")
+#         i += 1
+#     print(i)
+for i in range(1, 101):
+    if (i // 10 == 3 or i // 10 == 6 or i // 10 == 9) and (i % 10 == 3 or i % 10 == 6 or i % 10 == 9):
+        print("*")
+    elif i // 10 == 3 or i // 10 == 6 or i // 10 == 9:
+        print("*")
+    elif i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
+        print("**")
+    else:
+        print(i)
+# a = 0
+# while a < 100:
+#     a += 1
+#     if ((a%10)==3 or (a % 10) == 6 or (a%10)==9):
+#         if ((a//10)==3 or (a//10)==6 or (a//10)==9):
+#             print("**")
+#         else:
+#             print("*")
+#     elif((a%10)==3 or (a % 10) == 6 or (a%10)==9):
+#         print("*")
+#     else:
+#         print(a)
